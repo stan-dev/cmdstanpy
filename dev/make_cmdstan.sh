2d32205316b06baae5f7d486a07719f211f659d5
Marmaduke Woodman <marmaduke.woodman@univ-amu.fr>
2019-01-10 22:48:35 +0100

    travis cmdstan build debug info

--- dev/make_cmdstan.sh
+++ dev/make_cmdstan.sh
@@ -7,9 +7,13 @@ if [[ -d cmdstan && -f cmdstan/bin/stanc ]]; then
     exit 0
 fi
 
+set -eux
+ls -lha
+which g++
+which clang++
+
 curl -OL https://github.com/stan-dev/cmdstan/releases/download/v$CSVER/cmdstan-$CSVER.tar.gz
 tar xzf cmdstan-$CSVER.tar.gz
-rm -rf cmdstan-$CSVER.tar.gz
 mv cmdstan-$CSVER cmdstan
 cd cmdstan
 make -j2 build examples/bernoulli/bernoulli
