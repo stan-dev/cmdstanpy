80b59456f10b6006a39d9610117471b646bd85dd
Mitzi Morris <mitzi@panix.com>
2019-05-10 18:47:14 -0400

    lint fix

--- cmdstanpy/__init__.py
+++ cmdstanpy/__init__.py
@@ -4,7 +4,6 @@ import shutil
 import tempfile
 
 
-
 CMDSTAN_PATH = os.path.abspath(os.path.join('.', 'releases', 'cmdstan'))
 TMPDIR = tempfile.mkdtemp()
 STANSUMMARY_STATS = ['Mean', 'MCSE', 'StdDev', '5%', '50%', '95%',
