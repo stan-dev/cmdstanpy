debf01179a0a1b74958f403dc7e256b7b01b07af
Teddy Groves <tedgro@nnfcb-l0601.home>
2019-06-07 20:55:28 +0200

    Style: change += to append

--- cmdstanpy/cmds.py
+++ cmdstanpy/cmds.py
@@ -54,7 +54,7 @@ def compile_model(
         if include_paths is not None:
             if any([not os.path.exists(d) for d in include_paths]):
                 raise Exception('no such include path {}'.format(d))
-            cmd += ['--include_paths=' + ','.join(include_paths)]
+            cmd.append('--include_paths=' + ','.join(include_paths))
         print('stan to c++: make args {}'.format(cmd))
         do_command(cmd)
         if not os.path.exists(hpp_file):
