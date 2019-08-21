2e808212baa404e9fa75680c35157a6d37afbea4
Ari Hartikainen <ari.hartikainen@aalto.fi>
2019-08-21 16:36:26 +0300

    update CI

---
+++ test/example_script.py
@@ -0,0 +1,33 @@
+"""Example script to be runned after tests, tqdm installed."""
+import os
+import sys
+
+# explicit import to test if it is installed
+import tqdm
+
+from cmdstanpy import Model, cmdstan_path, set_make_env
+
+
+def run_bernoulli_fit():
+    # specify Stan file, create, compile Model object
+    bernoulli_path = os.path.join(
+        cmdstan_path(), 'examples', 'bernoulli', 'bernoulli.stan'
+    )
+    bernoulli_model = Model(stan_file=bernoulli_path)
+    bernoulli_model.compile()
+
+    # specify data, fit the model
+    bernoulli_data = {"N": 10, "y": [0, 1, 0, 0, 0, 0, 0, 0, 0, 1]}
+    # Show progress
+    bernoulli_fit = bernoulli_model.sample(
+        chains=4, cores=2, data=bernoulli_data, show_progress=True
+    )
+
+    # summarize the results (wraps CmdStan `bin/stansummary`):
+    print(bernoulli_fit.summary())
+
+
+if __name__ == "__main__":
+    run_bernoulli_fit()
+    # exit explicitly
+    sys.exit(0)
