149cfe86c9608e791c553dbccb864f095fa3c8fd
ahartikainen <ahartikainen@github.com>
2019-09-14 23:51:35 +0300

    update tests

--- test/test_cxx_installation.py
+++ test/test_cxx_installation.py
@@ -6,31 +6,64 @@ import platform
 import shutil
 
 from cmdstanpy import TMPDIR
-from cmdstanpy.install_cxx_toolchain import main as install_cxx
+from cmdstanpy import install_cxx_toolchain
 
 
 class install_cxx_script(unittest.TestCase):
-    def test_not_windows(self):
+    def test_config(self):
+        """Test config output."""
+        if platform.system() != "Windows":
+            return
+        else:
+            config = install_cxx_toolchain.get_config("C:\\RTools", True)
+
+            config_reference = [
+                '/SP-',
+                '/VERYSILENT',
+                '/SUPPRESSMSGBOXES',
+                '/CURRENTUSER',
+                'LANG="English"',
+                '/DIR="RTools"',
+                '/NOICONS',
+                '/NORESTART',
+            ]
+
+            self.assertEqual(config, config_reference)
+
+    def test_install_not_windows(self):
+        """Try to install on unsupported platform."""
         if platform.system() == "Windows":
             return
 
         with self.assertRaisesRegex(
             NotImplementedError,
-            'Download for the C++ toolchain on the current platform has not been implemented: \s+',
+            r'Download for the C++ toolchain on the current platform has not been implemented: \s+',
         ):
-            install_cxx()
+            install_cxx_toolchain.main()
 
-    @mock.patch(
-        'argparse.ArgumentParser.parse_args',
-        return_value=argparse.Namespace(version="3.5", dir=TMPDIR),
-    )
-    def test_windows(self, mock_args):
+    def test_normalize_version(self):
+        """Test supported versions."""
         if platform.system() != "Windows":
             return
+        else:
+            for ver in ['4.0', '4', '40']:
+                self.assertEqual(
+                    install_cxx_toolchain.normalize_version(ver), '4.0'
+                )
 
-        install_cxx()
-        self.assertTrue(os.path.exists(os.path.join(TMPDIR, "RTools")))
-        shutil.rmtree(os.path.join(TMPDIR, "RTools"), ignore_errors=True)
+            for ver in ['3.5', '35']:
+                self.assertEqual(
+                    install_cxx_toolchain.normalize_version(ver), '3.5'
+                )
+
+    def test_toolchain_name(self):
+        """Check toolchain name."""
+        if platform.system() != "Windows":
+            return
+        else:
+            self.assertEqual(
+                install_cxx_toolchain.get_toolchain_name(), "RTools"
+            )
 
 
 if __name__ == '__main__':
