e4f839649ffc58a70c79382ac64e16eb32aee813
ahartikainen <ahartikainen@github.com>
2019-09-15 01:21:46 +0300

    empty arrays to empty list

--- cmdstanpy/utils.py
+++ cmdstanpy/utils.py
@@ -193,9 +193,12 @@ def _rdump_array(key: str, val: np.ndarray) -> str:
 def jsondump(path: str, data: Dict) -> None:
     """Dump a dict of data to a JSON file."""
     for key, val in data.items():
-        if isinstance(val, np.ndarray) and val.size > 1:
-            # .T for Fortran order
-            data[key] = val.T.tolist()
+        if isinstance(val, np.ndarray):
+            if val.size > 1:
+                # .T for Fortran order
+                data[key] = val.T.tolist()
+            else:
+                data[key] = []
     with open(path, 'w') as fd:
         json.dump(data, fd)
 
