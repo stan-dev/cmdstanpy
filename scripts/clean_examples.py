83aa966121fbacf58d1ea7c55f9b814cbe7dad09
ahartikainen <ahartikainen@github.com>
2019-10-20 23:53:19 +0300

    fix typo

--- scripts/clean_examples.py
+++ scripts/clean_examples.py
@@ -4,7 +4,7 @@ from cmdstanpy import cmdstan_path
 
 def clean_examples():
     cmdstan_examples = s.path.join(cmdstan_path(), "examples")
-    for root, _, files in os.walk(cmdstan_examples)
+    for root, _, files in os.walk(cmdstan_examples):
         for filename in files:
             _, ext = os.path.splitext(filename)
             if ext.lower() in (".o", ".hpp", ".exe", ""):
