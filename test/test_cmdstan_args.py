71d414cf99bbf31e866ecf095215e5ef89719efa
Wes Barnett <wes.barnett@aexp.com>
2019-11-08 07:11:32 -0500

    Update tests to use cmd array and not string

--- test/test_cmdstan_args.py
+++ test/test_cmdstan_args.py
@@ -22,14 +22,16 @@ class OptimizeArgsTest(unittest.TestCase):
         self.assertRaises(ValueError, lambda: args.validate())
         args = OptimizeArgs(algorithm='Newton')
         args.validate()
-        cmd = args.compose(None, 'output')
-        self.assertIn('algorithm=newton', cmd)
+        cmd = args.compose(None, ['output'])
+        self.assertIn('algorithm=newton', ' '.join(cmd))
 
     def test_args_algorithm_init_alpha(self):
         args = OptimizeArgs(init_alpha=2e-4)
         args.validate()
-        cmd = args.compose(None, 'output')
-        self.assertIn('init_alpha=0.0002', cmd)
+        cmd = args.compose(None, ['output'])
+
+
+        self.assertIn('init_alpha=0.0002', ' '.join(cmd))
         args = OptimizeArgs(init_alpha=-1.0)
         self.assertRaises(ValueError, lambda: args.validate())
         args = OptimizeArgs(init_alpha=1.0, algorithm='Newton')
@@ -38,8 +40,8 @@ class OptimizeArgsTest(unittest.TestCase):
     def test_args_algorithm_iter(self):
         args = OptimizeArgs(iter=400)
         args.validate()
-        cmd = args.compose(None, 'output')
-        self.assertIn('iter=400', cmd)
+        cmd = args.compose(None, ['output'])
+        self.assertIn('iter=400', ' '.join(cmd))
         args = OptimizeArgs(iter=-1)
         self.assertRaises(ValueError, lambda: args.validate())
 
@@ -48,8 +50,8 @@ class SamplerArgsTest(unittest.TestCase):
     def test_args_min(self):
         args = SamplerArgs()
         args.validate(chains=4)
-        cmd = args.compose(idx=1, cmd='')
-        self.assertIn('method=sample algorithm=hmc', cmd)
+        cmd = args.compose(idx=1)
+        self.assertIn('method=sample algorithm=hmc', ' '.join(cmd))
 
     def test_args_chains(self):
         args = SamplerArgs()
@@ -66,23 +68,23 @@ class SamplerArgsTest(unittest.TestCase):
             adapt_delta=0.99,
         )
         args.validate(chains=4)
-        cmd = args.compose(1, '')
-        self.assertIn('method=sample', cmd)
-        self.assertIn('num_warmup=10', cmd)
-        self.assertIn('num_samples=20', cmd)
-        self.assertIn('save_warmup=1', cmd)
-        self.assertIn('thin=7', cmd)
-        self.assertIn('algorithm=hmc engine=nuts', cmd)
-        self.assertIn('max_depth=15 adapt delta=0.99', cmd)
+        cmd = args.compose(1)
+        self.assertIn('method=sample', ' '.join(cmd))
+        self.assertIn('num_warmup=10', ' '.join(cmd))
+        self.assertIn('num_samples=20', ' '.join(cmd))
+        self.assertIn('save_warmup=1', ' '.join(cmd))
+        self.assertIn('thin=7', ' '.join(cmd))
+        self.assertIn('algorithm=hmc engine=nuts', ' '.join(cmd))
+        self.assertIn('max_depth=15 adapt delta=0.99', ' '.join(cmd))
 
         args = SamplerArgs(warmup_iters=10)
         args.validate(chains=4)
-        cmd = args.compose(1, '')
-        self.assertIn('method=sample', cmd)
-        self.assertIn('num_warmup=10', cmd)
-        self.assertNotIn('num_samples=', cmd)
-        self.assertNotIn('save_warmup=', cmd)
-        self.assertNotIn('algorithm=hmc engine=nuts', cmd)
+        cmd = args.compose(1)
+        self.assertIn('method=sample', ' '.join(cmd))
+        self.assertIn('num_warmup=10', ' '.join(cmd))
+        self.assertNotIn('num_samples=', ' '.join(cmd))
+        self.assertNotIn('save_warmup=', ' '.join(cmd))
+        self.assertNotIn('algorithm=hmc engine=nuts', ' '.join(cmd))
 
     def test_bad(self):
         args = SamplerArgs(warmup_iters=-10)
@@ -152,61 +154,61 @@ class SamplerArgsTest(unittest.TestCase):
     def test_adapt(self):
         args = SamplerArgs(adapt_engaged=False)
         args.validate(chains=4)
-        cmd = args.compose(1, '')
-        self.assertIn('method=sample algorithm=hmc adapt engaged=0', cmd)
+        cmd = args.compose(1)
+        self.assertIn('method=sample algorithm=hmc adapt engaged=0', ' '.join(cmd))
 
         args = SamplerArgs(adapt_engaged=True)
         args.validate(chains=4)
-        cmd = args.compose(1, '')
-        self.assertIn('method=sample algorithm=hmc adapt engaged=1', cmd)
+        cmd = args.compose(1)
+        self.assertIn('method=sample algorithm=hmc adapt engaged=1', ' '.join(cmd))
 
         args = SamplerArgs()
         args.validate(chains=4)
-        cmd = args.compose(1, '')
-        self.assertNotIn('engine=nuts', cmd)
-        self.assertNotIn('engaged=1', cmd)
+        cmd = args.compose(1)
+        self.assertNotIn('engine=nuts', ' '.join(cmd))
+        self.assertNotIn('engaged=1', ' '.join(cmd))
 
     def test_metric(self):
         args = SamplerArgs(metric='dense_e')
         args.validate(chains=4)
-        cmd = args.compose(1, '')
-        self.assertIn('method=sample algorithm=hmc metric=dense_e', cmd)
+        cmd = args.compose(1)
+        self.assertIn('method=sample algorithm=hmc metric=dense_e', ' '.join(cmd))
 
         args = SamplerArgs(metric='dense')
         args.validate(chains=4)
-        cmd = args.compose(1, '')
-        self.assertIn('method=sample algorithm=hmc metric=dense_e', cmd)
+        cmd = args.compose(1)
+        self.assertIn('method=sample algorithm=hmc metric=dense_e', ' '.join(cmd))
 
         args = SamplerArgs(metric='diag_e')
         args.validate(chains=4)
-        cmd = args.compose(1, '')
-        self.assertIn('method=sample algorithm=hmc metric=diag_e', cmd)
+        cmd = args.compose(1)
+        self.assertIn('method=sample algorithm=hmc metric=diag_e', ' '.join(cmd))
 
         args = SamplerArgs(metric='diag')
         args.validate(chains=4)
-        cmd = args.compose(1, '')
-        self.assertIn('method=sample algorithm=hmc metric=diag_e', cmd)
+        cmd = args.compose(1)
+        self.assertIn('method=sample algorithm=hmc metric=diag_e', ' '.join(cmd))
 
         args = SamplerArgs()
         args.validate(chains=4)
-        cmd = args.compose(1, '')
-        self.assertNotIn('metric=', cmd)
+        cmd = args.compose(1)
+        self.assertNotIn('metric=', ' '.join(cmd))
 
         jmetric = os.path.join(datafiles_path, 'bernoulli.metric.json')
         args = SamplerArgs(metric=jmetric)
         args.validate(chains=4)
-        cmd = args.compose(1, '')
-        self.assertIn('metric=diag_e', cmd)
-        self.assertIn('metric_file=', cmd)
-        self.assertIn('bernoulli.metric.json', cmd)
+        cmd = args.compose(1)
+        self.assertIn('metric=diag_e', ' '.join(cmd))
+        self.assertIn('metric_file=', ' '.join(cmd))
+        self.assertIn('bernoulli.metric.json', ' '.join(cmd))
 
         jmetric2 = os.path.join(datafiles_path, 'bernoulli.metric-2.json')
         args = SamplerArgs(metric=[jmetric, jmetric2])
         args.validate(chains=2)
-        cmd = args.compose(0, '')
-        self.assertIn('bernoulli.metric.json', cmd)
-        cmd = args.compose(1, '')
-        self.assertIn('bernoulli.metric-2.json', cmd)
+        cmd = args.compose(0)
+        self.assertIn('bernoulli.metric.json', ' '.join(cmd))
+        cmd = args.compose(1)
+        self.assertIn('bernoulli.metric-2.json', ' '.join(cmd))
 
         args = SamplerArgs(metric=[jmetric, jmetric])
         with self.assertRaises(ValueError):
@@ -223,8 +225,8 @@ class SamplerArgsTest(unittest.TestCase):
     def test_fixed_param(self):
         args = SamplerArgs(fixed_param=True)
         args.validate(chains=1)
-        cmd = args.compose(0, '')
-        self.assertIn('method=sample algorithm=fixed_param', cmd)
+        cmd = args.compose(0)
+        self.assertIn('method=sample algorithm=fixed_param', ' '.join(cmd))
 
 
 class CmdStanArgsTest(unittest.TestCase):
@@ -283,10 +285,10 @@ class CmdStanArgsTest(unittest.TestCase):
         )
         self.assertEqual(cmdstan_args.method, Method.SAMPLE)
         cmd = cmdstan_args.compose_command(idx=0, csv_file='bern-output-1.csv')
-        self.assertIn('id=1 random seed=', cmd)
-        self.assertIn('data file=', cmd)
-        self.assertIn('output file=', cmd)
-        self.assertIn('method=sample algorithm=hmc', cmd)
+        self.assertIn('id=1 random seed=', ' '.join(cmd))
+        self.assertIn('data file=', ' '.join(cmd))
+        self.assertIn('output file=', ' '.join(cmd))
+        self.assertIn('method=sample algorithm=hmc', ' '.join(cmd))
 
         cmdstan_args = CmdStanArgs(
             model_name='bernoulli',
@@ -296,7 +298,7 @@ class CmdStanArgsTest(unittest.TestCase):
             method_args=sampler_args,
         )
         cmd = cmdstan_args.compose_command(idx=0, csv_file='bern-output-1.csv')
-        self.assertIn('id=7 random seed=', cmd)
+        self.assertIn('id=7 random seed=', ' '.join(cmd))
 
     def test_args_inits(self):
         exe = os.path.join(datafiles_path, 'bernoulli')
@@ -316,7 +318,7 @@ class CmdStanArgsTest(unittest.TestCase):
             method_args=sampler_args,
         )
         cmd = cmdstan_args.compose_command(idx=0, csv_file='bern-output-1.csv')
-        self.assertIn('init=', cmd)
+        self.assertIn('init=', ' '.join(cmd))
 
         cmdstan_args = CmdStanArgs(
             model_name='bernoulli',
@@ -327,9 +329,9 @@ class CmdStanArgsTest(unittest.TestCase):
             method_args=sampler_args,
         )
         cmd = cmdstan_args.compose_command(idx=0, csv_file='bern-output-1.csv')
-        self.assertIn('bernoulli.init_1.json', cmd)
+        self.assertIn('bernoulli.init_1.json', ' '.join(cmd))
         cmd = cmdstan_args.compose_command(idx=1, csv_file='bern-output-1.csv')
-        self.assertIn('bernoulli.init_2.json', cmd)
+        self.assertIn('bernoulli.init_2.json', ' '.join(cmd))
 
         cmdstan_args = CmdStanArgs(
             model_name='bernoulli',
@@ -340,7 +342,7 @@ class CmdStanArgsTest(unittest.TestCase):
             method_args=sampler_args,
         )
         cmd = cmdstan_args.compose_command(idx=0, csv_file='bern-output-1.csv')
-        self.assertIn('init=0', cmd)
+        self.assertIn('init=0', ' '.join(cmd))
 
         cmdstan_args = CmdStanArgs(
             model_name='bernoulli',
@@ -351,7 +353,7 @@ class CmdStanArgsTest(unittest.TestCase):
             method_args=sampler_args,
         )
         cmd = cmdstan_args.compose_command(idx=0, csv_file='bern-output-1.csv')
-        self.assertIn('init=3.33', cmd)
+        self.assertIn('init=3.33', ' '.join(cmd))
 
     def test_args_bad(self):
         sampler_args = SamplerArgs()
@@ -505,9 +507,9 @@ class GenerateQuantitesTest(unittest.TestCase):
         ]
         args = GenerateQuantitiesArgs(csv_files=csv_files)
         args.validate(chains=4)
-        cmd = args.compose(idx=1, cmd='')
-        self.assertIn('method=generate_quantities', cmd)
-        self.assertIn('fitted_params={}'.format(csv_files[0]), cmd)
+        cmd = args.compose(idx=1)
+        self.assertIn('method=generate_quantities', ' '.join(cmd))
+        self.assertIn('fitted_params={}'.format(csv_files[0]), ' '.join(cmd))
 
 class VariationalTest(unittest.TestCase):
     def test_args_variational(self):
@@ -516,15 +518,15 @@ class VariationalTest(unittest.TestCase):
 
         args = VariationalArgs(output_samples=1)
         args.validate(chains=1)
-        cmd = args.compose(idx=0, cmd='')
-        self.assertIn('method=variational', cmd)
-        self.assertIn('output_samples=1', cmd)
+        cmd = args.compose(idx=0)
+        self.assertIn('method=variational', ' '.join(cmd))
+        self.assertIn('output_samples=1', ' '.join(cmd))
 
         args = VariationalArgs(tol_rel_obj=1)
         args.validate(chains=1)
-        cmd = args.compose(idx=0, cmd='')
-        self.assertIn('method=variational', cmd)
-        self.assertIn('tol_rel_obj=1', cmd)
+        cmd = args.compose(idx=0)
+        self.assertIn('method=variational', ' '.join(cmd))
+        self.assertIn('tol_rel_obj=1', ' '.join(cmd))
 
     def test_args_bad(self):
         args = VariationalArgs(algorithm='no_such_algo')
