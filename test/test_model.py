299544fc4653d176ad126c2e9c988faa2c0a0cee
Mitzi Morris <mitzi@panix.com>
2019-12-01 23:11:08 -0500

    pylinting tests

--- test/test_model.py
+++ test/test_model.py
@@ -1,16 +1,18 @@
+"""CmdStanModel tests"""
+
 import os
-import pytest
 import unittest
+import pytest
 
 from cmdstanpy.utils import EXTENSION
 from cmdstanpy.model import CmdStanModel
 
 from cmdstanpy.utils import cmdstan_path
 
-here = os.path.dirname(os.path.abspath(__file__))
-datafiles_path = os.path.join(here, 'data')
+HERE = os.path.dirname(os.path.abspath(__file__))
+DATAFILES_PATH = os.path.join(HERE, 'data')
 
-code = '''data {
+CODE = """data {
   int<lower=0> N;
   int<lower=0,upper=1> y[N];
 }
@@ -22,17 +24,18 @@ model {
   for (n in 1:N)
     y[n] ~ bernoulli(theta);
 }
-'''
+"""
 
 
 class CmdStanModelTest(unittest.TestCase):
 
-    @pytest.fixture(scope="class", autouse=True)
+    # pylint: disable=no-self-use
+    @pytest.fixture(scope='class', autouse=True)
     def do_clean_up(self):
-        for root, _, files in os.walk(datafiles_path):
+        for root, _, files in os.walk(DATAFILES_PATH):
             for filename in files:
                 _, ext = os.path.splitext(filename)
-                if ext.lower() in (".o", ".hpp", ".exe", ""):
+                if ext.lower() in ('.o', '.hpp', '.exe', ''):
                     filepath = os.path.join(root, filename)
                     os.remove(filepath)
 
@@ -41,8 +44,8 @@ class CmdStanModelTest(unittest.TestCase):
         self.assertTrue(True)
 
     def test_model_good(self):
-        stan = os.path.join(datafiles_path, 'bernoulli.stan')
-        exe = os.path.join(datafiles_path, 'bernoulli' + EXTENSION)
+        stan = os.path.join(DATAFILES_PATH, 'bernoulli.stan')
+        exe = os.path.join(DATAFILES_PATH, 'bernoulli' + EXTENSION)
 
         # compile on instantiation
         model = CmdStanModel(stan_file=stan)
@@ -54,25 +57,21 @@ class CmdStanModelTest(unittest.TestCase):
         self.assertEqual(stan, model.stan_file)
         self.assertTrue(model.exe_file.endswith(exe))
 
+        # instantiate with existing exe only - no model
+        exe = os.path.join(DATAFILES_PATH, 'bernoulli' + EXTENSION)
+        model2 = CmdStanModel(exe_file=exe)
+        self.assertEqual(exe, model2.exe_file)
+        self.assertEqual('bernoulli', model2.name)
+        with self.assertRaises(RuntimeError):
+            model2.code()
+        with self.assertRaises(RuntimeError):
+            model2.compile()
+
         # instantiate, don't compile
         os.remove(exe)
         model = CmdStanModel(stan_file=stan, compile=False)
         self.assertEqual(stan, model.stan_file)
         self.assertEqual(None, model.exe_file)
-
-    def test_model_good_no_source(self):
-        stan = os.path.join(datafiles_path, 'bernoulli.stan')
-        exe = os.path.join(datafiles_path, 'bernoulli' + EXTENSION)
-        model = CmdStanModel(stan_file=stan)
-
-        model2 = CmdStanModel(exe_file=exe)
-        self.assertEqual(exe, model2.exe_file)
-        self.assertEqual('bernoulli', model2.name)
-
-        with self.assertRaises(RuntimeError):
-            model2.code()
-        with self.assertRaises(RuntimeError):
-            model2.compile()
 
     def test_model_none(self):
         with self.assertRaises(ValueError):
@@ -80,30 +79,30 @@ class CmdStanModelTest(unittest.TestCase):
 
     def test_model_bad(self):
         with self.assertRaises(Exception):
-            model = CmdStanModel(stan_file='xdlfkjx', exe_file='sdfndjsds')
+            CmdStanModel(stan_file='xdlfkjx', exe_file='sdfndjsds')
 
-        stan = os.path.join(datafiles_path, 'b')
+        stan = os.path.join(DATAFILES_PATH, 'b')
         with self.assertRaises(Exception):
-            model = CmdStanModel(stan_file=stan)
+            CmdStanModel(stan_file=stan)
 
-        stan = os.path.join(datafiles_path, 'bad_syntax.stan')
+        stan = os.path.join(DATAFILES_PATH, 'bad_syntax.stan')
         with self.assertRaises(Exception):
-            model = CmdStanModel(stan_file=stan)
+            CmdStanModel(stan_file=stan)
 
     def test_repr(self):
-        stan = os.path.join(datafiles_path, 'bernoulli.stan')
+        stan = os.path.join(DATAFILES_PATH, 'bernoulli.stan')
         model = CmdStanModel(stan_file=stan)
-        s = repr(model)
-        self.assertIn('name=bernoulli', s)
+        model_repr = repr(model)
+        self.assertIn('name=bernoulli', model_repr)
 
     def test_print(self):
-        stan = os.path.join(datafiles_path, 'bernoulli.stan')
+        stan = os.path.join(DATAFILES_PATH, 'bernoulli.stan')
         model = CmdStanModel(stan_file=stan)
-        self.assertEqual(code, model.code())
+        self.assertEqual(CODE, model.code())
 
     def test_model_compile(self):
-        stan = os.path.join(datafiles_path, 'bernoulli.stan')
-        exe = os.path.join(datafiles_path, 'bernoulli' + EXTENSION)
+        stan = os.path.join(DATAFILES_PATH, 'bernoulli.stan')
+        exe = os.path.join(DATAFILES_PATH, 'bernoulli' + EXTENSION)
 
         model = CmdStanModel(stan_file=stan)
         self.assertTrue(model.exe_file.endswith(exe.replace('\\', '/')))
@@ -119,16 +118,15 @@ class CmdStanModelTest(unittest.TestCase):
         # test compile with existing exe - timestamp on exe unchanged
         exe_time = os.path.getmtime(model.exe_file)
         model2 = CmdStanModel(stan_file=stan)
-        self.assertEqual(exe_time,os.path.getmtime(model2.exe_file))
+        self.assertEqual(exe_time, os.path.getmtime(model2.exe_file))
 
     def test_model_compile_includes(self):
-        stan = os.path.join(datafiles_path, 'bernoulli_include.stan')
-        exe = os.path.join(datafiles_path, 'bernoulli_include' + EXTENSION)
+        stan = os.path.join(DATAFILES_PATH, 'bernoulli_include.stan')
+        exe = os.path.join(DATAFILES_PATH, 'bernoulli_include' + EXTENSION)
         if os.path.exists(exe):
             os.remove(exe)
 
-        here = os.path.dirname(os.path.abspath(__file__))
-        datafiles_abspath = os.path.join(here, 'data')
+        datafiles_abspath = os.path.join(HERE, 'data')
         include_paths = [datafiles_abspath]
 
         # test compile with explicit include paths
@@ -137,8 +135,8 @@ class CmdStanModelTest(unittest.TestCase):
         self.assertTrue(model.exe_file.endswith(exe.replace('\\', '/')))
 
         # test compile - implicit include path is current dir
-        os.remove(os.path.join(datafiles_path, 'bernoulli_include' + '.hpp'))
-        os.remove(os.path.join(datafiles_path, 'bernoulli_include' + '.o'))
+        os.remove(os.path.join(DATAFILES_PATH, 'bernoulli_include' + '.hpp'))
+        os.remove(os.path.join(DATAFILES_PATH, 'bernoulli_include' + '.o'))
         os.remove(exe)
         model2 = CmdStanModel(stan_file=stan)
         self.assertEqual(model2.include_paths, include_paths)
