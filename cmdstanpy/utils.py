9ffca238e91b226bde02ebbf3acfe60e731fca4c
Brendon Oleary <brendon@seatgeek.com>
2020-08-26 14:31:10 -0400

    do not raise an error if eta adaptation doesn't happen

--- cmdstanpy/utils.py
+++ cmdstanpy/utils.py
@@ -553,20 +553,15 @@ def scan_variational_csv(path: str) -> Dict:
         lineno = scan_column_names(fd, dict, lineno)
         line = fd.readline().lstrip(' #\t').rstrip()
         lineno += 1
-        if not line.startswith('Stepsize adaptation complete.'):
-            raise ValueError(
-                'line {}: expecting adaptation msg, found:\n\t "{}"'.format(
-                    lineno, line
+        if line.startswith('Stepsize adaptation complete.'):
+            line = fd.readline().lstrip(' #\t\n')
+            lineno += 1
+            if not line.startswith('eta'):
+                raise ValueError(
+                    'line {}: expecting eta, found:\n\t "{}"'.format(
+                        lineno, line
+                    )
                 )
-            )
-        line = fd.readline().lstrip(' #\t\n')
-        lineno += 1
-        if not line.startswith('eta'):
-            raise ValueError(
-                'line {}: expecting eta, found:\n\t "{}"'.format(
-                    lineno, line
-                )
-            )
         line = fd.readline().lstrip(' #\t\n')
         lineno += 1
         xs = line.split(',')
