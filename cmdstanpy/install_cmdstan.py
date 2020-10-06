a9ed7c04bc6b0efbddba1a558511e6a1440c4f80
Mitzi Morris <mitzi@panix.com>
2020-10-06 00:42:11 -0400

    investigate install hang on windows

--- cmdstanpy/install_cmdstan.py
+++ cmdstanpy/install_cmdstan.py
@@ -95,7 +95,7 @@ def install_version(
             while proc.poll() is None:
                 if verbose:
                     print(proc.stdout.readline().decode('utf-8').strip())
-            stdout, stderr = proc.communicate()
+            _, stderr = proc.communicate()
             if proc.returncode:
                 msgs = ['Command "make clean-all" failed']
                 if stderr:
@@ -113,7 +113,7 @@ def install_version(
         while proc.poll() is None:
             if verbose:
                 print(proc.stdout.readline().decode('utf-8').strip())
-        stdout, stderr = proc.communicate()
+        _, stderr = proc.communicate()
         if proc.returncode:
             msgs = ['Command "make build" failed']
             if stderr:
