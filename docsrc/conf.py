1e1c8a2061388b5672e9e8faae17334b3e369cb5
Mitzi Morris <mitzi@panix.com>
2021-06-10 13:23:11 -0400

    remove sphinx version req

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -26,7 +26,7 @@ sys.path.insert(0, os.path.dirname(os.path.abspath('.')))
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-needs_sphinx = '4.0'
+# needs_sphinx = '4.0'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
