5675456a3faadd25ea014e2c339ec5b793f4954a
Brian Ward <bward@flatironinstitute.org>
2021-07-30 12:36:37 -0400

    Check for built binaries, resolves #396

--- cmdstanpy/install_cmdstan.py
+++ cmdstanpy/install_cmdstan.py
@@ -127,6 +127,16 @@ def install_version(
             if stderr:
                 msgs.append(stderr.decode('utf-8').strip())
             raise CmdStanInstallError('\n'.join(msgs))
+        if not os.path.exists(os.path.join('bin', 'stansummary' + EXTENSION)):
+            raise CmdStanInstallError(
+                f"bin/stansummary{EXTENSION} not found"
+                ", please rebuild or report a bug!"
+            )
+        if not os.path.exists(os.path.join('bin', 'diagnose' + EXTENSION)):
+            raise CmdStanInstallError(
+                f"bin/stansummary{EXTENSION} not found"
+                ", please rebuild or report a bug!"
+            )
         print('Test model compilation')
         cmd = [
             make,
