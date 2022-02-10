b5fb0977eed07baacbeff0f3a1f336ce6931dd67
Mitzi Morris <mitzi@panix.com>
2022-02-10 00:02:24 -0500

    complex conversion OK, needs unit tests

--- cmdstanpy/stanfit/mcmc.py
+++ cmdstanpy/stanfit/mcmc.py
@@ -31,6 +31,7 @@ except ImportError:
 from cmdstanpy import _CMDSTAN_SAMPLING, _CMDSTAN_THIN, _CMDSTAN_WARMUP, _TMPDIR
 from cmdstanpy.cmdstan_args import Method, SamplerArgs
 from cmdstanpy.utils import (
+    BaseType,
     EXTENSION,
     check_sampler_csv,
     cmdstan_path,
@@ -670,12 +671,30 @@ class CmdStanMCMC:
         num_draws = self.num_draws_sampling
         if inc_warmup and self._save_warmup:
             num_draws += self.num_draws_warmup
-        dims = [num_draws * self.chains]
+        num_draws_tot = num_draws * self.chains
+        dims = [num_draws_tot]
         col_idxs = self._metadata.stan_vars_cols[var]
-        if len(col_idxs) > 0:
+        if len(self._metadata.stan_vars_dims[var]) > 0:
             dims.extend(self._metadata.stan_vars_dims[var])
-        # pylint: disable=redundant-keyword-arg
-        return self._draws[draw1:, :, col_idxs].reshape(dims, order='F')
+        if self._metadata.stan_vars_types[var] != BaseType.COMPLEX:
+            # pylint: disable=redundant-keyword-arg
+            return self._draws[draw1:, :, col_idxs].reshape(dims, order='F')
+        else:
+            dims.pop()
+            draws_complex = np.empty(
+                (math.prod(dims)), dtype=complex, order='F'
+            )
+            # pylint: disable=redundant-keyword-arg
+            slice = self._draws[draw1:, :, col_idxs].reshape(
+                (num_draws_tot, len(col_idxs)), order='F'
+            )
+            idx = 0
+            for i in range(num_draws_tot):
+                it = iter(slice[i, :])
+                for x in it:
+                    draws_complex[idx] = x + 1j * next(it)
+                    idx += 1
+            return draws_complex.reshape(dims, order='F')
 
     def stan_variables(self) -> Dict[str, np.ndarray]:
         """
--- test/data/complex_var.stan
+++ test/data/complex_var.stan
@@ -1,4 +1,7 @@
 generated quantities {
-  array[2,3] int xs = {{3,4,0},{1,2,3}};
-  array[2,3] complex zs = {{3,4i,0},{1i,2i,3i}};
+  int a  = 1;
+  array[2,3,2] int ys = {{{3,0},{0,4}, {5,0}}, {{0,1}, {0,2}, {0,3}}};
+  array[2,3] complex zs = {{3,4i,5},{1i,2i,3i}};
+  print(ys);
+  print(zs);
 }
