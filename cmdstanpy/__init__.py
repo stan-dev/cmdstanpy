97d5afb3b77a836474bac34cc9ae93b93dac9f9d
Mitzi Morris <mitzi@panix.com>
2022-08-11 11:41:51 -0400

    lint fix

--- cmdstanpy/__init__.py
+++ cmdstanpy/__init__.py
@@ -2,7 +2,6 @@
 """CmdStanPy Module"""
 
 import atexit
-import logging
 import shutil
 import tempfile
 
@@ -16,9 +15,7 @@ _DOT_CMDSTAN = '.cmdstan'
 
 def _cleanup_tmpdir() -> None:
     """Force deletion of _TMPDIR."""
-    # logging.getLogger('cmdstanpy').info('deleting tmpfiles dir: %s', _TMPDIR)
     shutil.rmtree(_TMPDIR, ignore_errors=True)
-    # logging.getLogger('cmdstanpy').info('done')
 
 
 atexit.register(_cleanup_tmpdir)
