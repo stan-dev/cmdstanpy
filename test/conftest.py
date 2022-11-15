caf946b63fe6ba8db420187cd1ff13c38e81a20a
Till Hoffmann <tillahoffmann@gmail.com>
2022-11-15 15:27:38 -0500

    Use `session` scope for removing artifacts.

--- test/conftest.py
+++ test/conftest.py
@@ -8,9 +8,9 @@ HERE = os.path.dirname(os.path.abspath(__file__))
 DATAFILES_PATH = os.path.join(HERE, 'data')
 
 
-@pytest.fixture(autouse=True)
+@pytest.fixture(scope='session', autouse=True)
 def cleanup_test_files():
-    """Remove compiled models and output files after each test run."""
+    """Remove compiled models and output files after test run."""
     yield
     subprocess.Popen(
         ['git', 'clean', '-fX', DATAFILES_PATH],
